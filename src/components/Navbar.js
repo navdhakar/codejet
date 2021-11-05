83e596a2e87be25ad573cef240fb33deb1ef8ebe
navdeep <navdepdhakar1@gmail.com>
2021-11-05 19:11:13 +0530

    jlnbkhgh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -37,6 +37,7 @@ function Navbar() {
                 </Link>
               </li>
             </ul>
+            <h4>if you are a college student:</h4>
             <form className="form-inline my-2 my-lg-0">
               <Link to="/login" className="btn btn-outline-dark my-2 my-sm-0 mr-3 text-uppercase">
                 login
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -4,49 +4,25 @@ import { Link } from "react-router-dom";
 function Signup() {
   return (
     <div>
-      <div
-        className="container-fluid"
-        style={{ marginTop: "80px", alignItems: "center" }}
-        id="gtco-footer"
-      >
+      <div className="container-fluid" style={{ marginTop: "80px", alignItems: "center" }} id="gtco-footer">
         <div className="container">
           <div className="row">
             <div className="col" id="contact">
-              <h4>Register </h4>
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Username"
-              />
-              <input
-                type="email"
-                className="form-control"
-                placeholder="Email Address"
-              />
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Password"
-              />
-              <input
-                type="password"
-                className="form-control"
-                placeholder="Confirm password"
-              />
+              <h4>Student Register </h4>
+              <input type="email" className="form-control" placeholder="Username" />
+              <input type="email" className="form-control" placeholder="Email Address" />
+              <input type="password" className="form-control" placeholder="Password" />
+              <input type="password" className="form-control" placeholder="Confirm password" />
               <a href="/" className="submit-button">
                 Sign-Up <i className="fa fa-angle-right" aria-hidden="true" />
               </a>
               <h5 style={{ marginTop: "20px" }}>
-                <Link to="/login">Already had an account?</Link>
+                <Link to="/login">Already have an account?</Link>
               </h5>
             </div>
             <div className="col-md-6">
               <div className="card">
-                <img
-                  className="card-img-top img-fluid"
-                  src="images/banner-img.png"
-                  alt=""
-                />
+                <img className="card-img-top img-fluid" src="images/banner-img.png" alt="" />
               </div>
             </div>
           </div>
