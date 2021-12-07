c3b588dff2fd32ca40d8c320875527c0644fee57
navdeep <navdepdhakar1@gmail.com>
2021-12-07 17:54:37 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const server = process.env.NODE_ENV == "production" ? "https://codejet.herokuapp
 function App() {
   useEffect(() => {
     ReactGa.initialize("G-H96HSG3SJ0");
-    ReactGa.pageview(window.location.pathname + window.location.search);
+    ReactGa.pageview("/");
   }, []);
   return (
     <Router>
