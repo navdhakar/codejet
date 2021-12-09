8afc09797fecfe4f67dcc54dada96d5689b37bef
Mohit7891 <mohitjain.cs23@gmail.com>
2021-12-09 20:14:40 +0530

    Edit terms and conditions

--- src/components/terms.js
+++ src/components/terms.js
@@ -6,71 +6,89 @@ export default function terms() {
     <div style={{ margin: "100px" }}>
       <div className="row" style={{ paddingBottom: "30px" }}>
         <div className="col-md-5">
-          <h3 style={{ paddingBottom: "30px" }}>Terms & Conditions</h3>
-          <h4 style={{ paddingBottom: "30px" }}>UNPAID INTERNSHIP👇</h4>
+          <h3 style={{ paddingBottom: "30px" }}><b><u>Terms & Conditions :</u></b></h3>
+          <h4 style={{ paddingBottom: "30px" }}>PAID / UNPAID INTERNSHIP👇</h4>
           {/* <p style={{ paddingBottom: "20px" }}> */}
             {" "}
             <h4>💡About the internship</h4><br></br>
           {/* </p> */}
           {/* <p style={{ paddingBottom: "30px" }}> */}
-<b>Completion Certificate:</b>Awarded after successful completion of one month of internship.<br></br>
-<b>Recommendation letter:</b>Will be provided to interns who performed really well in the tasks allotted.<br></br>
-<b>Workshop/Guidance:</b> Will set the opportunity to work under supervision and chances to attend several interactive sessions.<br></br>
-<b>Network Opportunity:</b> Multicultural diverse working environment is available for interns.We will post your profile on our website.This will help you to grow your network and finding more job & freelancing opportunities.<br></br>
+<b>Completion Certificate:</b> Will be awarded after successful completion of internship.<br></br>
+<br></br>
+<b>Recommendation letter:</b> Will be provided to interns who performed really well in all the tasks allotted.<br></br>
+<br></br>
+<b>Workshop/Guidance:</b> Will set the opportunity to work under supervision and chances to attend several Industry level live interactive sessions.<br></br>
+<br></br>
+<b>Network Opportunity:</b> Will set the chances to connect with other developers.We will post your profile on our website.This will help you to grow your network and finding more job & freelancing opportunities.<br></br>
 <br></br>
 
 
           {/* </p> */}
           {/* <p>By hiring us you will not only have best IT services at your disposal but you will become part of great cause and revolution.</p> */}
           <h4>💡Details information</h4><br></br>
-          <b>UNPAID INTERNSHIP:</b>This is an unpaid internship of one month during which you’ll be trained, mentored, and observed for future roles. You’ll be given opportunities to increase your exposure within the team and will also learn from the mentors.<br></br>
-          <b>FLEXIBLE WORKING HOURS:</b>During the course of the internship you will be expected to work from home with the flexibility to manage your time. The tasks assigned are easy and can be finished in your extra time.<br></br>
-          <b>WORK FROM HOME:</b>You’ll use your own computers with internet connections and coordinate with your team members through Whatsapp.<br></br>
+          <b>UNPAID INTERNSHIP:</b> During the unpaid internship you’ll be trained, mentored, and observed for future roles.You’ll be given opportunities to learn industry level experience within the team and will also learn from the mentors.<br></br>
+          <br></br>
+          <b>PAID INTERNSHIP:</b> During the paid internship you’ll be trained, mentored, and observed for future roles.you have to create a demo project.if our client agreed with your project, after that you will be selected for paid internship.There is no guarantee of selection or payment.<br></br>
+          <br></br>
+          <b>FLEXIBLE WORKING HOURS:</b> During the course of the internship you will be expected to work from home with the flexibility to manage your time. The tasks assigned are easy and can be finished in your extra time.<br></br>
+          <br></br>
+          <b>WORK FROM HOME:</b> You’ll use your own computers with internet connections and coordinate with your team members through Whatsapp.<br></br>
           <br></br>
           <h4>💡LEARNING THROUGH THIS INTERNSHIP</h4><br></br>
 
-          <b>SCHEDULED CALLS AND MEETINGS:</b> Regular contact with core team members via live online sessions.<br></br>
+          <b>SCHEDULED CALLS AND MEETINGS:</b> Regular contact with core team members via social media & interactive live online sessions.<br></br>
+          <br></br>
           <b>MENTOR STAND:</b> Our mentors are there to guide and support you at any time.You can reach the mentors through email or whatsapp.<br></br>
-          <b>ON DEMAND SUPPORT:</b> you feel, you can mail us for help. We will guide you to the correct resources.<br></br>
+          <br></br>
+          <b>ON DEMAND SUPPORT:</b> You can mail us for help at any time. We will guide you to the correct resources.
         </div>
-        <h4 style={{ paddingBottom: "30px" }}>UNPAID INTERNSHIP👇</h4>
-
       </div>
+    
       <div className="row" style={{ paddingBottom: "30px" }}>
         <div className="col-md-5">
-          <h1 style={{ paddingBottom: "20px" }}>Why us </h1>
-          <p style={{ paddingBottom: "20px" }}>● We provide very affordable prices in compare to other IT services providers</p>
-          <p style={{ paddingBottom: "20px" }}>
-            ● We want to save students and IT service requirer like you from those middleman who charge absurd prices to you for their services and pay very
-            less to students they hire in name of internship.
-          </p>
-          <p style={{ paddingBottom: "20px" }}>
-            ● We connect developers directly to recruiter by eliminating third party middleman and cutting cost of operations hence saving your money.
-          </p>
-          <p style={{ paddingBottom: "20px" }}>● We hire highly skilled student developer who work under guidance of industry experts.</p>
-          <p style={{ paddingBottom: "20px" }}>
-            ● as our developers are students who are always learning new tech. services provided by our developers will be highly oriented with ever evolving
-            new technology and so will be your product.
-          </p>
-
-          <p style={{ paddingBottom: "20px" }}>● you will be hiring new and fresh mindset of developer which is open to new opportunities and hard tasks.</p>
-          <p style={{ paddingBottom: "20px" }}>
-            ● If you have a startup idea, you can find highly motivated and tech enthusiastic developers to take it to the next big step.
-          </p>
-          <p style={{ paddingBottom: "20px" }}>
-            ● We are not like other service based company , our team of developer is focussed solely on your project providing best service and support for you.
-          </p>
-          <p style={{ paddingBottom: "20px" }}>So what are you waiting for 🤗!!</p>
-          <div className="container-fluid gtco-banner-area" style={{ marginBottom: "-500px" }}>
+          {/* <h1 style={{ paddingBottom: "20px" }}>Why us </h1> */}
+          <h3 style={{ paddingBottom: "20px" }}>Conditions:</h3>
+          ● The internship is related to an educational purpose and there is no guarantee or expectation that the activity will result in employment with the Company.<br></br>
+<br></br>
+         
+            ● The education received by the Intern from the internship is for the express benefit of the Intern.<br></br>
+<br></br>
+        
+            ● The education received by the Intern from the internship is for the express benefit of the Intern.<br></br>
+<br></br>
+            ● The Company does not derive an immediate advantage from the activities performed by the Intern.<br></br>
+<br></br> 
+            ● Intern is not entitled to wages or any compensation or benefits for the time spent in the internship.<br></br>
+<br></br>
+           ●Company is not liable for injury sustained or health conditions that may arise for the unpaid intern during the course of the internship.<br></br>
+<br></br>      
+            ● This internship is educational in nature and there is no guarantee or expectation that the internship will result in employment.<br></br>
+<br></br>
+            ● Company may at any time in its sole discretion, terminate the internship without notice or cause.<br></br>
+            <br></br>
+            ● Intern will demonstrate honesty, punctuality, courtesy, cooperative attitude, proper health and grooming habits, appropriate dress and a willingness to learn.<br></br>
+            <br></br>
+            ● Intern will obey the policies, rules and regulations of the Company site and comply with the Company’s business practices and procedures.<br></br>
+            <br></br>
+            ● Intern will furnish his/her supervisor with all necessary information pertaining to my unpaid internship, including related assignments and reports.<br></br>
+            <br></br>
+          {/* <p style={{ paddingBottom: "20px" }}> */}
+            ● While Intern is on the Company premises, he/she is considered an employee or agent of Company for any purposes, including but not limited to workers compensation.<br></br>
+          {/* </p> */}
+          {/* <p style={{ paddingBottom: "20px" }}>
+            ● Intern assumes all of the risks of participating in the internship program. In consideration of the opportunity afforded to the Intern to participate in the internship program, Intern hereby agrees that he/she, his/her assignees, heirs, guardians, and legal representatives, will not make a claim against Company or any of its affiliated organizations, or either of their officers or directors collectively or individually, or any of its employees, for the injury of death to Intern or damage to his/her property, however caused, arising from his/her participation in the internship program. Without limiting the generality of the foregoing. Intern hereby waives and releases any rights, actions, or causes or action resulting from personal injury or death to him/her, or damage to his/her property, sustained in connection with his/her participation in the internship program.
+          </p> */}
+          {/* <p style={{ paddingBottom: "20px" }}>So what are you waiting for 🤗!!</p> */}
+          {/* <div className="container-fluid gtco-banner-area" style={{ marginBottom: "-500px" }}>
             <div className="container">
               <div className="row">
                 <div className="col-md-12">
-                  <div className="navnik">
+                  <div className="navnik"> */}
                     {/* <Link to="/opportunity" className="mohirti" style={{ marginRight: "30px" }}>
                   Find Work
                   <i className="fa fa-angle-right" aria-hidden="true" />
                 </Link> */}
-                    <Link to="/contact" className="mohirti" style={{ marginRight: "30px", marginLeft: "-35px" }}>
+                    {/* <Link to="/contact" className="mohirti" style={{ marginRight: "30px", marginLeft: "-35px" }}>
                       Contact us
                       <i className="fa fa-angle-right" aria-hidden="true" />
                     </Link>
@@ -82,7 +100,7 @@ export default function terms() {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
