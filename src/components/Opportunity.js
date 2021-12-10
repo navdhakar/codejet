f78211fd9606d6437891f588826e6d6456a35a2d
navdeep <navdepdhakar1@gmail.com>
2021-12-10 19:16:17 +0530

    wef

--- src/components/Opportunity.js
+++ src/components/Opportunity.js
@@ -210,38 +210,20 @@ function Opportunity() {
             Projects
           </p>
 
-
           <div className="row">
-                <div className="col" >
-
-                    <Link className="btn btn-primary" to="/Opportunity" style={{float: "right" ,marginbottom: "5%"}}>
-                      Free Internship 
-                      {/* <i className="fa fa-angle-right" aria-hidden="true" /> */}
-
-          <div className="container-fluid gtco-banner-area" style={{ marginBottom: "-500px" }}>
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8">
-                  <div className="navnit" align="center" text-align="center">
-                    {/* <Link to="/opportunity" className="mohirti" style={{ marginRight: "30px" }}>
-                  Find Work
-                  <i className="fa fa-angle-right" aria-hidden="true" />
-                </Link> */}
-                    <Link to="/Opportunity" style={{ marginRight: "30px", marginLeft: "-35px" }}>
-                      Free Internship
-                      <i className="fa fa-angle-right" aria-hidden="true" />
-
-                    </Link>
-                    </div>
-                <div className="col" >
-
-                    <Link to="/paid" className="btn btn-primary">
-                      Paid Internship
-                      {/* <i className="fa fa-angle-right" aria-hidden="true" /> */}
-                    </Link>
-                    </div>
-                
-              </div>
+            <div className="col">
+              <Link className="btn btn-primary" to="/Opportunity" style={{ float: "right", marginbottom: "5%" }}>
+                Free Internship
+                {/* <i className="fa fa-angle-right" aria-hidden="true" /> */}
+              </Link>
+            </div>
+            <div className="col">
+              <Link to="/paid" className="btn btn-primary">
+                Paid Internship
+                {/* <i className="fa fa-angle-right" aria-hidden="true" /> */}
+              </Link>
+            </div>
+          </div>
 
           <div id="courses-container" className="max-width-container">
             <div id="MostPopular" className="category-container">
